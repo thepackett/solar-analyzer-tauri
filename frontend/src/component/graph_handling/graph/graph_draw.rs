b72385c513b0393859c6f6c518f68fc42ac0b08d
David Hackett <david.w.hackett0@gmail.com>
2023-10-27 01:21:31 -0500

    Updating Graph Draw WIP

--- frontend/src/component/graph_handling/graph/graph_draw.rs
+++ frontend/src/component/graph_handling/graph/graph_draw.rs
@@ -1,6 +1,8 @@
-use plotters::prelude::*;
+use std::ops::Range;
+use plotters::{prelude::*, coord::{ranged1d::{ValueFormatter, NoDefaultFormatting, KeyPointHint}, types::RangedCoordf64}};
 use plotters_canvas::CanvasBackend;
-use shared::graph::{graph_type::GraphType, graph_axis::AxisDataType};
+use shared::graph::{graph_type::GraphType, graph_axis::{AxisDataType, AxisDataOption}};
+use time::OffsetDateTime;
 use yew::Context;
 
 use crate::component::visual::theme_data::ThemeData;
@@ -10,14 +12,95 @@ use super::{Graph, graph_draw_utils::{other_axis_label_formatter, time_axis_labe
 pub const CHART_MARGIN_SIZE: u32 = 20;
 pub const CHART_LABEL_SIZE: u32 = 30;
 
+struct GraphDataRange {
+    pub range: RangedCoordf64,
+    pub data_type: AxisDataType,
+}
+
+impl Ranged for GraphDataRange {
+    type FormatOption = NoDefaultFormatting;
+    type ValueType = f64;
+
+    fn map(&self, value: &Self::ValueType, limit: (i32, i32)) -> i32 {
+        self.range.map(value, limit)
+    }
+
+    fn key_points<Hint: KeyPointHint>(&self, hint: Hint) -> Vec<Self::ValueType> {
+        match self.data_type {
+            AxisDataType::Time | AxisDataType::PeriodicTime => {
+                let start = self.range.range().start;
+                let end = self.range.range().end;
+                //We want even divisions of:
+                //1 Year,
+                //1 Month,
+                //1 Week,
+                //1 Day,
+                //6 Hour,
+                //1 Hour,
+                //15 Minutes,
+                //5 Minutes,
+                //All of these line up as nice subdivisions of the larger unit, with the exception of weeks
+                //There are a fractional number of weeks per month, and furthermore not all months have the same number of days.
+                //We want to show two of these divisions at all times, in both bold and regular lines. For example, if years are bold, then months are normal
+                //If Months are bold, then weeks are normal.
+                //If 15 minutes are bold, then 5 minutes are normal. Etc.
+                //Transition from one unit to a smaller unit if less than 1 whole unit is visible.
+                //Transition from one unit to a larger unit if at least 1 whole larger unit is visible.
+                let key_points: Vec<f64> = Vec::new();
+                
+                match end - start {
+                    x if x >= (60*60*24*365) as f64 => { //If we contain at least a whole year* (365 days)
+                        let start_date_time = OffsetDateTime::from_unix_timestamp(end as i64).unwrap_or(OffsetDateTime::UNIX_EPOCH);
+                    },
+                    x if x >= (60*60*24*30) as f64 => { //If we contain at least a whole month* (30 days)
+
+                    },
+                    x if x >= (60*60*24*7) as f64 => { //If we contain at least a whole week
+
+                    },
+                    x if x >= (60*60*24) as f64 => { //If we contain at least a whole day
+
+                    },
+                    x if x >= (60*60*6) as f64 => { //If we contain at least 6 hours
+
+                    },
+                    x if x >= (60*60) as f64 => { //If we contain at least 1 hour
+
+                    },
+                    _ => { //Assume we contain at least 15 minutes, since there are no smaller subdivision pairs left
+
+                    }
+                }
+                vec![0f64, 1f64*10000f64, 2f64*10000f64, 3f64*10000f64, 4f64*10000f64, 5f64*10000f64, 6f64*10000f64, 7f64*10000f64, 8f64*10000f64, 9f64*10000f64]
+            },
+            _ => self.range.key_points(hint),
+        }
+    }
+
+    fn range(&self) -> std::ops::Range<Self::ValueType> {
+        self.range.range()
+    }
+}
+
+impl ValueFormatter<f64> for GraphDataRange {
+    fn format(_value: &f64) -> String {
+        RangedCoordf64::format(_value)
+    }
+
+    fn format_ext(&self, value: &f64) -> String {
+        Self::format(value)
+    }
+}
+
 impl Graph {
     pub fn draw_graph(&mut self, ctx: &Context<Graph>, theme: ThemeData) -> Result<(), Box<dyn std::error::Error>>  {
         //Thinking about the broader picture, I really need to divide behavior based on graph type.
         //Currently 2 graph types are planned.
         //1. X axis time line series
-        //2. X-Y scatter plot
+        //2. X axis periodic time line series
+        //3. X-Y scatter plot
         //Most behavior can be shared between these plot types (graph mesh / captions, data ranges, styles, zoom controls, etc)
-        //But some behavior can't. For example, X-Y scatters must be drawn without a line. "Vertical line" markpoints only make sense for the x axis time line series.
+        //But some behavior can't. For example, X-Y scatters must be drawn without a line. "Vertical line" markpoints only make sense for the x axis line series.
 
 
 
@@ -140,7 +223,13 @@ impl Graph {
             .caption(format!("temp caption"), caption_font.clone().with_color(RGBColor::from(&theme.theme_text)))
             .x_label_area_size(CHART_LABEL_SIZE)
             .y_label_area_size(CHART_LABEL_SIZE)
-            .build_cartesian_2d(x_axis_range.clone(), y_axis_range.clone())?;
+            .build_cartesian_2d(GraphDataRange {
+                range: RangedCoordf64::from(x_axis_range.clone()),
+                data_type: self.graph_state.x_axis.requests.first().unwrap_or(&(AxisDataType::Time, AxisDataOption::Average)).0.clone(),
+            }, GraphDataRange{
+                range: RangedCoordf64::from(y_axis_range.clone()),
+                data_type: self.graph_state.y_axis.0.requests.first().unwrap_or(&(AxisDataType::BatteryVoltage, AxisDataOption::Average)).0.clone(),
+            })?;
 
         
         //chart.set_secondary_coord(x_axis_range, secondary_y_axis_range).configure_secondary_axes();
@@ -320,7 +409,8 @@ impl Graph {
             },
             None => "",
         };
-    
+
+
         chart.configure_mesh()
             .light_line_style(&RGBColor::from(&theme.theme_graph_mesh_light))
             .bold_line_style(&RGBColor::from(&theme.theme_graph_mesh_dark))
@@ -335,10 +425,12 @@ impl Graph {
             .y_label_formatter(&y_axis_formatter)
             .label_style(&RGBColor::from(&theme.theme_text))
             .draw()?;
+
         chart.configure_series_labels()
             .label_font(label_font.clone().color(&RGBColor::from(&theme.theme_text)))
             .background_style(&RGBColor::from(&theme.theme_graph_background))
             .border_style(&RGBColor::from(&theme.theme_graph_border))
+            .position(SeriesLabelPosition::UpperRight)
             .draw()?;
 
         //Draw and configure secondary axis, if it is present
